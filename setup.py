aee13ae877e0a949eada2f59f242ce9bf320298e
vognev <vitaliy.ognev@gmail.com>
2017-09-16 11:23:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 setup(
   name='dhns',
   version='0.0.1',
-  install_requires=requirements,
+  install_requires=['dnslib == 0.9.6', 'cachetools == 1.1.5', 'docker-py'],
   scripts=['main.py'],
   packages=['dhcplib', 'dhcpsrv', 'dhns', 'dnssrv', 'multiplexer']
 )
