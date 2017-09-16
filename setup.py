06975ef79c7b8e1b9619f1f166599582330a21aa
Vitaliy Ognev <vitaliy.ognev@gmail.com>
2017-09-16 11:41:31 +0300

    upd

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
-import os
 from setuptools import setup
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
 
 setup(
   name='dhns',
   version='0.0.1',
   install_requires=['dnslib == 0.9.6', 'cachetools == 1.1.5', 'docker-py'],
   scripts=['main.py'],
-  packages=['dhcplib', 'dhcpsrv', 'dhns', 'dnssrv', 'multiplexer']
+  packages=['dhcplib', 'dhcpsrv', 'dhns', 'dnssrv', 'multiplexer', 'resolvconf']
 )
